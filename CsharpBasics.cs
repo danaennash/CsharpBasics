3ece811ae4f402eff50021b60db28d03e28c30ce
stlfinestnae <36461588+stlfinestnae@users.noreply.github.com>
2019-02-22 16:36:32 -0600

    Update CsharpBasics.cs

--- CsharpBasics.cs
+++ CsharpBasics.cs
@@ -21,44 +21,48 @@ namespace ConsoleApp4
             String greeting="Hello", name="Karen";
 
             if (sample1 == sample2)
-            {
-                Console.WriteLine("The samples are equal.");
-            }
-            else
-            {
-                Console.WriteLine("The samples are not equal.");
-            }
+                {
+                    Console.WriteLine("The samples equal.");
+                }
+                else
+                {
+                    Console.WriteLine("The samples not equal.");
+                }
        
             if (heartRate>=40 && heartRate<=80)
-            {
-                Console.WriteLine("Heart rate is normal.");
-            }
-            else
-            {
-                Console.WriteLine("Heart rate is not normal.");
-            }
+                {
+                    Console.WriteLine("Heart rate normal.");
+                }
+                else
+                {
+                    Console.WriteLine("Heart rate is not normal.");
+                }
+            
             if (deposits>= 100000000)
-            {
-                Console.WriteLine("You are exceedingly wealthy.");
-            }
-            else
-            {
-                Console.WriteLine("Sorry you are so poor.");
-            }
+                {
+                    Console.WriteLine("You are exceedingly wealthy.");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry you are so poor.");
+                }
 
             double force = mass * acceleration;
+            
             Console.WriteLine("Force=" + force.ToString("#.000"));
             
             Console.WriteLine(distance + " is the distance.");
             
-             if(lost==true && expensive == true)
-            {
-                Console.WriteLine("I am really sorry! I will get the manager.");
-            }
-            else if(lost == true && expensive == false)
-            {
-                Console.WriteLine("Here is coupon for 10% off.");
-            }
+            
+            if(lost==true && expensive == true)
+                {
+                    Console.WriteLine("I am really sorry! I will get the manager.");
+                }
+                else if(lost == true && expensive == false)
+                {
+                    Console.WriteLine("Here is coupon for 10% off.");
+                }
+            
             
             switch (choice)
             {
@@ -79,17 +83,17 @@ namespace ConsoleApp4
            
             Console.WriteLine(integral + "is an integral.");
         
-            for(int i = 5; i <= 10; i++)
-            {
-                Console.WriteLine("i = " + i);
-            }
+                for(int i = 5; i <= 10; i++)
+                {
+                    Console.WriteLine("i = " + i);
+                }
         
             int age = 0;
             while (age < 6)
-            {
-                Console.WriteLine("age = " + age);
-                age++;
-            }
+                {
+                    Console.WriteLine("age = " + age);
+                    age++;
+                }
           
             Console.WriteLine(greeting+" "+name);
         }
